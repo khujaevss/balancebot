cebb0c1c48a8ead7d58c080a4720fdc4280c80d0
Chase Spring <samkh7007@gmail.com>
2026-07-07 13:47:01 +0100

    Add MPU5060 tilt angle sketch

---
+++ arduino/MPU6050_final.ino
@@ -0,0 +1,21 @@
+#include <MPU6050.h>
+#include <Wire.h>
+
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(38400);
+  Wire.begin();
+  mpu.initialize();
+}
+
+void loop() {
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  float accelAngle = atan2((float)ay, (float)az) * 180.0 / PI;
+
+  Serial.println(accelAngle);
+
+  delay(50);
+}
