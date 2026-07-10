e53f7d625ee39a2c021563b97f3d18913421cc27
Chase Spring <samkh7007@gmail.com>
2026-07-10 12:39:37 +0100

    Add accelerometer-only test sketch for noise/vibration experiment

--- arduino/MPU6050_final/MPU6050_final.ino
+++ arduino/MPU6050_final/MPU6050_final.ino
@@ -2,26 +2,19 @@
 #include <Wire.h>
 
 MPU6050 mpu;
-float gyroAngle = 0;
-unsigned long lastTime = 0;
 
 void setup() {
   Serial.begin(38400);
   Wire.begin();
   mpu.initialize();
-  lastTime = micros();
 }
 
 void loop() {
   int16_t ax, ay, az, gx, gy, gz;
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  float gyroRate = gz / 131.0;
-  unsigned long now = micros();
-  float dt = (now - lastTime) / 1000000.0;
-  lastTime = now;
+  float accelAngle = atan2((float)ay, (float)az) * 180.0 / PI;
 
-  gyroAngle += gyroRate * dt;
-  Serial.println(gyroAngle);
-  delay(10);
+  Serial.println(accelAngle);
+  delay(50);
 }
